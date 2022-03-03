97404b5f985ca90235ea967eb1d6cbf2b202517b
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2022-03-03 16:08:26 +0100

    klar med allt

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -18,6 +18,8 @@ export class HomeComponent implements OnInit {
 
   searchResult: IProduct[] = []
 
+  checkoutItems: IProduct[] = []
+
   constructor(private service: GetproductsService) { }
 
   ngOnInit(): void {
@@ -31,7 +33,22 @@ export class HomeComponent implements OnInit {
 
     this.service.getCategoriesFromApi();
     this.service.getProducts();
+
+    /////////////////////////////////////  localstorage /////////////////////////////////////////////
+    let checkoutItemsSerialized: string = localStorage.getItem("checkoutItems") || "[]";
+    this.checkoutItems = JSON.parse(checkoutItemsSerialized);
+    //////////////////////////////////////////////////////////////////////////////////////////////////
     
+  }
+
+  attItemToCheckout(item: IProduct) {
+
+    this.checkoutItems.push(item);
+    localStorage.setItem("checkoutItems", JSON.stringify(this.checkoutItems));
+
+    this.service.updateBasketItemNumber(this.checkoutItems.length)
+    localStorage.setItem("amountOfItemsInBasket", JSON.stringify(this.checkoutItems.length))
+
   }
 
   searchProduct(usersSearch:string){
